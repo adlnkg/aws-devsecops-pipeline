08253474198e00831f33ee15f456b1b0674283f9
adlnkg <adlene.koroghli@gmail.com>
2026-09-04 19:27:39 +0200

    feat(observability): integrate prometheus scraping and grafana dashboard

--- api/app.py
+++ api/app.py
@@ -1,16 +1,21 @@
-from fastapi import FastAPI
-import socket
 import datetime
+import socket
+from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI(title="Cloud API")
+
+Instrumentator().instrument(app).expose(app)
+
 
 @app.get("/")
 def root():
     return {
         "message": "Service OP",
-        "timestamp":datetime.datetime.utcnow().isoformat(),
-        "hostname": socket.gethostname()
+        "timestamp": datetime.datetime.now(datetime.timezone.utc).isoformat(),
+        "hostname": socket.gethostname(),
     }
+
 
 @app.get("/health")
 def health():
